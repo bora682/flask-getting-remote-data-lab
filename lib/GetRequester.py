5658bf53fab4b04580193cb3983a5dfc6492fead
Bora <bora@christinesmbp16.mynetworksettings.com>
2025-11-08 22:32:52 -0500

    Implement GetRequester class with working methods

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,15 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.content 
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return json.loads(response.text) 
